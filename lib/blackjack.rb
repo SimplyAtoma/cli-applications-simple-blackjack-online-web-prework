60ab7cb66da5f035b359b3d89c99ccb5f20de126
Hector Rodriguez Duenas <h.rodri831@gmail.com>
2019-08-11 20:13:31 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -21,8 +21,9 @@ def get_user_input
   # code #get_user_input here
 end
 
-def end_game
+def end_game(total)
   # code #end_game here
+  puts "Sorry, you hit #{total}. Thanks for playing!"
 end
 
 def initial_round
